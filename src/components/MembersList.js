971c1fed7a9dd28f4fa46d9e8f0bc2497a885be0
Zhixuan Zhou <francesca@shuus-MacBook-puro.local>
2020-10-27 21:16:03 +0800

    [zhixuan] modify memberslist

--- src/components/MembersList.js
+++ src/components/MembersList.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import Card from './Card'
+import Card from './Card';
 
 class MembersList extends Component {
-    render () {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h2>学员列表</h2>
+        <section>
+          <Card id="1" name="成吉思汗" />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default MembersList;
